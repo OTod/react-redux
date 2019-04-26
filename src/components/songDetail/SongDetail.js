fa80c59262b2e2be149c7a7d5fa31f2411b4ea64
Oleksandr <oleksandr.todorov@globallogic.com>
2019-04-26 15:37:16 +0300

    small refactor in song detail

--- src/components/songDetail/SongDetail.js
+++ src/components/songDetail/SongDetail.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail =(props) => {
-    console.log(props);
+const SongDetail =({song}) => {
 
-    if(!props.song) return <div>Please, select a song.</div>
+    if(!song) return <div>Please, select a song.</div>
     return (
     <div>
-        <h3>{props.song.title}</h3>
-        <span>Duration: {props.song.duration}</span>
+        <h3>{song.title}</h3>
+        <span>Duration: {song.duration}</span>
     </div>
     )
 };
