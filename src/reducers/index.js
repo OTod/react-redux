a177ad65be7d43d977d6b7814ca832ea021f4eef
Oleksandr <oleksandr.todorov@globallogic.com>
2019-04-26 12:46:11 +0300

    actions, reducers added

---
+++ src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'Memoru Remains', duration: '5:43' },
+        { title: 'Mama Said', duration: '5:23' },
+        { title: 'Whiskey in the jar', duration: '4:43' },
+        { title: 'Saint anger', duration: '3:43' },
+    ]
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
