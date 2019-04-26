3893c436f80f2a1c11aedf76240bd0ed0cad992c
Oleksandr <oleksandr.todorov@globallogic.com>
2019-04-26 13:01:15 +0300

    added song list component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { selectSong } from '../../actions/index';
+import SongList from '../songList/SongList';
 
 const App = () => {
-    return <div>App</div>
+    return (
+    <div>
+        <SongList />   
+    </div>);
 };
 
 export default App;
---
+++ src/components/songList/SongList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+export default class SongList extends React.Component {
+    render(){
+        return <div> This is a song list component</div>
+    }
+}
